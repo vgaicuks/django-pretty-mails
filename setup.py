13750f99445db383668c51cd70e9c9e51881e88c
Vitālijs Gaičuks <vitalijs.gaicuks@gmail.com>
2018-10-24 16:27:58 +0300

    setup.py fix

--- setup.py
+++ setup.py
@@ -6,14 +6,15 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='django_pretty_mails',
-    version='0.0.2',
+    version='0.0.3',
     author='Vitālijs Gaičuks',
     author_email='vitalijs.gaicuks@gmail.com',
     description='Django templated mails with responsive html design.',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/vgaicuks/django-pretty-mails',
-    packages=setuptools.find_packages(),
+    packages=['django_pretty_mails'],
+    include_package_data=True,
     requires=['django(>=1.11)'],
     classifiers=[
         "Programming Language :: Python :: 3",
