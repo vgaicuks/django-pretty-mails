6c5eae190690af03b3614fe62d109123a88978f6
Vitālijs Gaičuks <vitalijs.gaicuks@gmail.com>
2018-10-24 16:19:42 +0300

    manifest.in file

--- setup.py
+++ setup.py
@@ -13,9 +13,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/vgaicuks/django-pretty-mails',
-    # packages=setuptools.find_packages(),
-    packages=['django_pretty_mails'],
-    include_package_data=True,
+    packages=setuptools.find_packages(),
     requires=['django(>=1.11)'],
     classifiers=[
         "Programming Language :: Python :: 3",
