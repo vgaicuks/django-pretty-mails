fffd372a7e74bc1fd4dba84001bc711e50edfdc9
Vitālijs Gaičuks <vitalijs.gaicuks@gmail.com>
2020-03-20 11:49:41 +0200

    Bump version up

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='django_pretty_mails',
-    version='0.0.8',
+    version='0.0.9',
     author='Vitālijs Gaičuks',
     author_email='vitalijs.gaicuks@gmail.com',
     description='Django templated mails with responsive html design.',
